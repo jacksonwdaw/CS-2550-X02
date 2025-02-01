4c47c61f925144e0440374970ea1fe124cf505c8
jacksonwdaw <jacksonwdaw@gmail.com>
2025-01-31 22:37:50 -0700

    added modal js

---
+++ scripts/modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("myModal");
+var img = document.getElementById("portrait");
+var modalImg = document.getElementById("modalImg");
+var closeBtn = document.getElementById("closeBtn");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt; 
+    document.getElementById("caption").innerHTML = this.alt; 
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none"; 
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none"; 
+    }
+}
